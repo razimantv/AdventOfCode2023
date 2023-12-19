5fc2d04a773aa72d2e1ce62851043a2f56b9a943
Raziman T V <razimantv@gmail.com>
2023-12-19 00:52:58 +0000

    Advent of Code: Day 7

---
+++ 7/1.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+labels = 'AKQJT98765432'
+values = {c: i for i, c in enumerate(labels[::-1])}
+
+hands = []
+with open('1.2.in') as file:
+    for line in file:
+        hand, val = line[:-1].split(' ')
+        hands.append([
+            sorted(Counter(hand).values(), reverse=True),
+            [values[c]for c in hand], int(val)
+        ])
+
+print(sum(
+    (i + 1) * value
+    for i, (_, _, value) in enumerate(sorted(hands))
+))
---
+++ 7/2.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+labels = 'AKQT98765432J'
+values = {c: i for i, c in enumerate(labels[::-1])}
+
+
+def best(hand):
+    cards = set(hand)
+    if 'J' in cards and len(cards) > 1:
+        cards.remove('J')
+    else:
+        return sorted(Counter(hand).values(), reverse=True)
+
+    for i, c in enumerate(hand):
+        if c == 'J':
+            return max(best(hand[:i] + cc + hand[i+1:]) for cc in cards)
+
+
+hands = []
+with open('1.2.in') as file:
+    for line in file:
+        hand, val = line[:-1].split(' ')
+        print(hand, best(hand))
+        hands.append([
+            best(hand), [values[c]for c in hand], int(val)
+        ])
+
+print(sum(
+    (i + 1) * value
+    for i, (_, _, value) in enumerate(sorted(hands))
+))
